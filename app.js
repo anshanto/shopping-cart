315f247dcd8a8dfd8d0537d007fe573ab7b8da3f
anshanto <asikurjamann@gmail.com>
2022-06-22 02:14:37 +0600

    update html

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ function updateProductNumber(product, price, isIncreasing) {
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
-    // update case total
+    // update total
     const caseTotal = document.getElementById(product + '-total');
     caseTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
 };
+function getInputValue(product) {
+    const ProductInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(ProductInput.value);
+    return productNumber;
+};
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+    // update html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = total;
+};
+
 // phone increase decrease event
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateProductNumber('phone', 1912, true);
