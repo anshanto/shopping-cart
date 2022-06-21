68d07264761f6b10c3043b0bc30b15997bdd7a00
anshanto <asikurjamann@gmail.com>
2022-06-21 17:49:45 +0600

    case number update

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('I am in a separate file')
+function updateCaseNumber(isIncreasing) {
+    const caseInput = document.getElementById('case-number');
+    let caseNumber = caseInput.value;
+    if (isIncreasing == true) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total
+    const caseTotal = document.getElementById('case-total');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    updateCaseNumber(true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    // const caseInput = document.getElementById('case-number');
+    // const caseNumber = caseInput.value;
+    updateCaseNumber(false);
+})
